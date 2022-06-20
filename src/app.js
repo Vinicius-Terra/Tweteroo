03f03f74ac576b81b5aca2325b8f9d59e50a9776
ViniciusTerra <viniterra33@gmail.com>
2022-06-20 16:28:23 -0300

    add res.status andget one users tweets

--- src/app.js
+++ src/app.js
@@ -12,23 +12,50 @@ const tweets = [];
 
 app.post('/sign-up', (req, res) => {
   const userInfo = req.body; 
+  if(!userInfo.username||!userInfo.avatar){
+    res.status(400).send("Todos os campos são obrigatórios!");
+    return;
+  }
   users.push(userInfo);
-  res.send("ok");
-  console.log(users)
+  return res.status(201).json(userInfo)
 });
 
+// to post a tweet
 app.post('/tweets', (req, res) => {
+  const {user} = req.headers;
   const tweet = req.body; 
-  const userInfo= users.find((obj)=>obj.username === tweet.username)
-  tweets.unshift({...tweet, avatar: userInfo.avatar})
-  res.send("ok");
-  console.log(tweet)
+
+  if(!user||!tweet.tweet){
+    res.status(400).send("Todos os campos são obrigatórios!");
+    return;
+  }
+
+  const userInfo= users.find((obj)=>obj.username === user)
+  tweets.unshift({...tweet, username:user, avatar: userInfo.avatar})
+
+  return res.status(201).json({
+    tweet:
+    {
+        username:user,
+        tweet:tweet.tweet,
+        avatar:userInfo.avatar,
+    },
+  })
 });
 
-
+// to get all tweeets
 app.get('/tweets', (req, res) => {
   const topTenTweets = tweets.slice(0, 10);
   res.send(topTenTweets);
 });
+
+
+// to get the tweeets of a specific user
+app.get('/tweets/:userName', (req, res) => {
+  const userName = req.params.userName;
+  const myTweets = tweets.filter((obj)=>obj.username === userName);
+  res.send(myTweets);
+});
+
 
 app.listen(5000);
